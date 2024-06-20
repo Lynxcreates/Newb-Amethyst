8f9933e0af862364c49e303c38bed9c0a198fc31
Lynxcreates <lynixbriefzking@gmail.com>
2024-06-20 18:54:11 +0200

    Update pack_config.sh

--- include/newb/pack_config.sh
+++ include/newb/pack_config.sh
@@ -8,16 +8,22 @@ DEFAULT_MATERIALS="RenderChunk Clouds Sky EndSky LegacyCubemap Actor SunMoon"
 #  NAMES     = Names/descriptions for options
 #  MATERIALS = Materials to compile for options
 SUBPACK_OPTIONS=(
+  FANTASY
+  HORROR
   FULL
   R
   DEFAULT
 )
 SUBPACK_NAMES=(
+  "Fantasy"
+  "Horror"
  "Full"
   "R"
   "Default"
 )
 SUBPACK_MATERIALS=(
+  "RenderChunk ; Clouds ;  Sky ; EndSky ; LegacyCubemap"
+  "RenderChunk ; Clouds ;  Sky ; EndSky ; LegacyCubemap"
   "Clouds"
   "RenderChunk ; Sky"
   ""
