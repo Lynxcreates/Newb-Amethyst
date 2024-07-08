7a49d5e2e75fb379acf96d83af7ade0ce3a5bcab
Lynxcreates <lynixbriefzking@gmail.com>
2024-07-08 14:13:44 +0200

    Update sky.h

--- src/newb/functions/sky.h
+++ src/newb/functions/sky.h
@@ -92,26 +92,26 @@ vec3 getSunBloom(float viewDirX, vec3 horizonEdgeCol, vec3 FOG_COLOR) {
 
 
 vec3 renderEndSky(vec3 horizonCol, vec3 zenithCol, vec3 viewDir, float t) {
-  t *= 0.1;
-  float a = atan2(viewDir.x, viewDir.z);
+  t *= 0.9;
+  float a = atan(viewDir.x, viewDir.z);
 
-  float n1 = 0.5 + 0.5*sin(3.0*a + t + 10.0*viewDir.x*viewDir.y);
+  float n1 = 0.5 + 0.5*sin(5.0*a + t + 20.0*viewDir.x*viewDir.y);
   float n2 = 0.5 + 0.5*sin(5.0*a + 0.5*t + 5.0*n1 + 0.1*sin(40.0*a -4.0*t));
-    
-  float waves = 0.7*n2*n1 + 0.3*n1;
-    
-  float grad = 0.5 + 0.5*viewDir.y;
-  float streaks = waves*(1.0 - grad*grad*grad);
-  streaks += (1.0-streaks)*smoothstep(1.0-waves, -1.0, viewDir.y);
 
-  float f = 0.3*streaks + 0.7*smoothstep(1.0, -0.5, viewDir.y);
+  float waves = 1.0*n2*n1 + 1.0*n1;
+
+  float grad = 0.0 + 0.4*viewDir.y;
+  float streaks = waves*(1.0 - grad*grad*grad*grad);
+  streaks += (1.0-streaks)*smoothstep(1.0-waves, -2.0, viewDir.y);
+
+  float f = 0.3*streaks + 0.7*smoothstep(2.0, -0.5, viewDir.y);
   float h = streaks*streaks;
   float g = h*h;
   g *= g;
-    
+
   vec3 sky = mix(zenithCol, horizonCol, f*f);
-  sky += (0.1*streaks + 2.0*g*g*g + h*h*h)*vec3(2.0,0.5,0.0);
-  sky += 0.25*streaks*spectrum(sin(2.0*viewDir.x*viewDir.y+t));
+  sky += (0.9*streaks + 9.0*g*g*g*1.5*h*g*g*g + h*h*h)*vec3(0.0,0.0,0.0);
+  sky += 0.4*h*streaks*spectrum(sin(2.0*viewDir.x*viewDir.y+t));
 
   return sky;
 }
